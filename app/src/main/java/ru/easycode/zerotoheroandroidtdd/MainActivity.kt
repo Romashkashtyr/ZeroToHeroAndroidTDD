bf148259e6ef69e3136ed4d3dfccf0435744ca83
Romashka <romainshtyr@yandex.ru>
2024-01-07 14:24:30 +0300

    task/009Test remove textview solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,44 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var linearLayout:LinearLayout
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linearLayout = findViewById(R.id.rootLayout)
+
+        textView = findViewById(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.removeButton)
+
+        button.setOnClickListener {
+            linearLayout.removeView(textView)
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val removedTextView = linearLayout.childCount == 1
+        outState.putBoolean(KEY, removedTextView)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val removedTextView = savedInstanceState.getBoolean(KEY)
+        if(removedTextView)
+            linearLayout.removeView(textView)
+    }
+
+    companion object{
+        private const val KEY = "key"
     }
 }
